4ba890416e8f3148e52c96ed263ffd321e519fef
abhishek poshwal <abhishek.poshwal.com>
2023-05-05 13:51:13 +0530

    third commit

--- shop.php
+++ shop.php
@@ -69,7 +69,7 @@ if(isset($_POST['add_to_cart'])){
       <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
       <div class="name"><?php echo $fetch_products['name']; ?></div>
       <div class="price">₹<?php echo $fetch_products['price']; ?>/-</div>
-      <input type="number" min="1" name="product_quantity" value="1" class="qty" style="text-align: center">
+      <input type="hidden" min="1" name="product_quantity" value="1" class="qty" style="text-align: center">
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
